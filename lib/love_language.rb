51d7205fd000afc985e2f8093da4a9d2c8ff02d1
solomonfy <fantahunsolomon@gmail.com>
2020-06-17 14:45:44 -0400

    6_17_20

--- lib/love_language.rb
+++ lib/love_language.rb
@@ -2,14 +2,22 @@ class LoveLanguage < ActiveRecord::Base
   has_many :stud_love_languages
   has_many :students, through: :stud_love_languages
 
-  def self.most_common_lovelanguage
-    common_lovel = StudLoveLanguage.group(:type).group(:love_language_id).order('count(love_language_id) desc').limit(3).pluck(:love_language_id)
+  def self.most_common_lovelanguage(give_get)
+    if_cond = if give_get.downcase == "Give".downcase || "Get".downcase
+    common_lovel = StudLoveLanguage.where(type: give_get).group(:type).group(:love_language_id).order('count(love_language_id) desc').limit(3).pluck(:love_language_id)
     common_lovel_name = LoveLanguage.find(common_lovel).pluck(:name).join(", ")
+    else
+      try_again = "Try again!"
+      puts try_again
+    end
+    if_cond
   end
 
-  def self.least_common_lovelanguage
-    rare_lovel = StudLoveLanguage.group(:love_language_id && :type).order('count(love_language_id) asc').limit(3).pluck(:love_language_id)
-    rare_lovel_name = LoveLanguage.find(rare_lovel).pluck(:name).join(", ")
+  def self.least_common_lovelanguage(give_get)
+    if give_get.downcase == "Give".downcase || "Get".downcase
+      rare_lovel = StudLoveLanguage.where(type: give_get).group(:type).group(:love_language_id).order("count(love_language_id) ASC").limit(3).pluck(:love_language_id)
+      rare_lovel_name = LoveLanguage.find(rare_lovel).pluck(:name).join(", ")
+    end
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,44 @@ class Student < ActiveRecord::Base
   has_many :love_languages, through: :stud_love_languages
   belongs_to :personality
 
-  def have_pets
-
+  def self.have_siblings
+    have_siblings = Student.where(siblings: true)
+    pp have_siblings.map {|a| "#{a.first_name} #{a.last_name}"}.join (", ")
   end
+
+  def self.have_no_siblings
+    no_siblings = Student.where(siblings: false)
+    pp no_siblings.map {|a| "#{a.first_name} #{a.last_name}"}.join (", ")
+  end
+
+  def self.have_pets_object
+    Student.where(pets: true)
+  end
+
+  def self.have_pets_string
+    p Student.have_pets_object.map {|a| "#{a.first_name} #{a.last_name}"}.join (", ")
+  end
+
+  def self.have_no_pets_object
+    Student.where(pets: false)
+  end
+
+  def self.have_no_pets_string
+    p Student.have_no_pets_object.map {|a| "#{a.first_name} #{a.last_name}"}.join (", ")
+  end
+
+  def self.pets_majority
+    ans = Student.have_pets_object.count <=> Student.have_no_pets_object.count
+      if ans == -1
+        p "There are more students without pets!"
+      elsif ans == 0
+        p "Students with AND without pets is the same!"
+      elsif ans == 1
+        p "There are more students with pets!"
+      end
+  end
+  
+  
+
 
 end
